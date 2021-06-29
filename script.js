c1a07ff9d7aad28a2e8252e568926d052ca1dc20
iainaitken <iain_aitken@yahoo.co.uk>
2021-06-29 10:08:49 +0100

    Change ? to number on successful guess; prevent game logic from executing if number already guessed

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 
 // Generate random number
 
+let guessed = false;
 let randomNumber = Math.ceil(Math.random() * 20);
 console.log(randomNumber);
 
@@ -9,16 +10,19 @@ console.log(randomNumber);
 
 document.querySelector('.check').addEventListener('click', function () {
   let input = Number(document.querySelector('.guess').value);
-
-  // if no input, change message to "oops"
-  if (input === 0) {
+  // check state
+  if (guessed) {
+    document.querySelector('.message').textContent = 'You already won!';
+  } else if (input === 0) {
     document.querySelector('.message').textContent = 'Enter a number!';
   } else if (input < 1 || input > 20) {
     document.querySelector('.message').textContent = 'Between 1 and 20!';
   } else if (input === randomNumber) {
     document.querySelector('.message').textContent = '🥳 Correct! 🥳';
+    document.querySelector('.number').textContent = randomNumber;
     document.querySelector('.highscore').textContent =
       document.querySelector('.score').textContent;
+    guessed = true;
   } else {
     document.querySelector('.score').textContent -= 1;
     if (input < randomNumber) {
